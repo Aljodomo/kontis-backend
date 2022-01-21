c286ce6e2d821a9f501a74de215b20e27fba6ae9
a.domonell <a.domonell@gipa.de>
2022-01-21 17:20:43 +0100

    Rename - ListUtils to StringUtils

--- src/main/java/com/aljodomo/kontis/utils/StringUtils.java
+++ src/main/java/com/aljodomo/kontis/utils/StringUtils.java
@@ -1,11 +1,11 @@
-package com.aljodomo.kontis.tagger;
+package com.aljodomo.kontis.utils;
 
 import java.util.List;
 
 /**
  * @author Aljoscha Domonell
  */
-public class ListUtils {
+public class StringUtils {
     public static String concat(List<String> words) {
         StringBuilder builder = new StringBuilder();
         words.forEach(s -> builder.append(s).append(" "));
