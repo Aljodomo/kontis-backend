85289a1dc0f0f781c7a00c11fab3e004177fbc57
a.domonell <a.domonell@gipa.de>
2022-01-21 17:17:49 +0100

    Add - DirectionRemoverTest

--- src/test/java/com/aljodomo/kontis/tagger/DirectionRemoverTest.java
+++ src/test/java/com/aljodomo/kontis/tagger/DirectionRemoverTest.java
@@ -1,6 +1,9 @@
 package com.aljodomo.kontis.tagger;
 
 import com.aljodomo.kontis.loader.StopLoader;
+import com.aljodomo.kontis.nlp.preperation.DirectionRemover;
+import com.aljodomo.kontis.nlp.preperation.MessageNormalizer;
+import com.aljodomo.kontis.utils.StringUtils;
 import net.ricecode.similarity.DiceCoefficientStrategy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,20 +11,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Aljoscha Domonell
  */
 @SpringBootTest
 class DirectionRemoverTest {
-    private final ArrayList<String> stopNames;
+    private final List<String> stopNames;
     private final MessageNormalizer messageNormalizer;
     DirectionRemover directionRemover;
 
     @Autowired
     public DirectionRemoverTest(StopLoader stopLoader, MessageNormalizer messageNormalizer) {
-        this.directionRemover = new DirectionRemover(new DiceCoefficientStrategy()); // osloer str = 0.43
-        this.stopNames = new ArrayList<>(stopLoader.getStations().keySet());
+        this.directionRemover = new DirectionRemover(new DiceCoefficientStrategy(), stopLoader); // osloer str = 0.43
+        this.stopNames = stopLoader.getStopNames();
         this.messageNormalizer = messageNormalizer;
     }
 
@@ -30,7 +34,8 @@ class DirectionRemoverTest {
         String message = "U9 Hansaplatz richtung Osloer 3 mänlich gelesen und 2weiblich mit schwarzen jacken und dunkle Haare";
         String expected = "U9 Hansaplatz 3 mänlich gelesen und 2weiblich mit schwarzen jacken und dunkle Haare";
         String nMessage = this.messageNormalizer.normalize(message);
-        String clearedMessage = this.directionRemover.removeDirections(stopNames, nMessage);
-        Assertions.assertEquals(messageNormalizer.normalize(expected), clearedMessage);
+        ArrayList<String> words = new ArrayList<>(List.of(nMessage.split(" ")));
+        List<String> removeDirections = this.directionRemover.removeDirections(words);
+        Assertions.assertEquals(messageNormalizer.normalize(expected), StringUtils.concat(words));
     }
 }
