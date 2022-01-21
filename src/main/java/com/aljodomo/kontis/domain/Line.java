702dae46ecebaa570cf79bb1b9e47e211570f2c9
a.domonell <a.domonell@gipa.de>
2022-01-21 16:25:44 +0100

    Add - Line class

---
+++ src/main/java/com/aljodomo/kontis/domain/Line.java
@@ -0,0 +1,13 @@
+package com.aljodomo.kontis.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Aljoscha Domonell
+ */
+@Data
+@AllArgsConstructor
+public class Line {
+    String name;
+}
