60b65bd6c58ed5dbdbb9094fef0fd94703adfbb3
a.domonell <a.domonell@gipa.de>
2022-01-21 17:22:48 +0100

    Add - getStopNames method

--- src/main/java/com/aljodomo/kontis/loader/StopLoader.java
+++ src/main/java/com/aljodomo/kontis/loader/StopLoader.java
@@ -2,11 +2,13 @@ package com.aljodomo.kontis.loader;
 
 import com.aljodomo.kontis.domain.Stop;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author Aljoscha Domonell
  */
 public interface StopLoader {
-    Map<String, Stop> getStations();
+    Map<String, Stop> getStops();
+    List<String> getStopNames();
 }
