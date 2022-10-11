ee70fee1ce65878316b6e6f7576cef6b3b10cc49
Aljoscha Domonell <aljo.domo@gmail.com>
2022-10-11 22:37:58 +0200

    feat: save report endpoint

--- src/main/java/com/aljodomo/kontis/controller/DataController.java
+++ src/main/java/com/aljodomo/kontis/controller/DataController.java
@@ -1,19 +1,20 @@
 package com.aljodomo.kontis.controller;
 
-import com.aljodomo.kontis.gtfs.GTFSService;
 import com.aljodomo.kontis.gtfs.ReportService;
 import com.aljodomo.kontis.model.Report;
-import com.aljodomo.kontis.persistence.InMemoryRepository;
+import com.aljodomo.kontis.persistence.ReportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.time.Duration;
 import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
-import java.util.stream.Collectors;
+
+import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.ok;
 
 /**
  * @author Aljoscha Domonell
@@ -24,37 +25,28 @@ import java.util.stream.Collectors;
 public class DataController {
 
     @Autowired
-    InMemoryRepository inMemoryDB;
+    ReportRepository reportRepository;
 
     @Autowired
     ReportService reportService;
-
-    @Autowired
-    GTFSService gtfsService;
-
-    @GetMapping("/activeReports")
-    public ResponseEntity<List<Report>> activeReports() {
-        return ResponseEntity.ok().body(this.inMemoryDB.findActive());
-    }
-
-    @PostMapping("/future")
-    public ResponseEntity<List<Report>> buildFuture(@RequestBody String message) {
-        ZonedDateTime time = ZonedDateTime.now();
-        var r = reportService.analyse(message, time);
-
-        return r.map(report -> ResponseEntity.ok().body(
-                        gtfsService.findReachable(report.getStopTime(), time, Duration.of(10, ChronoUnit.MINUTES))
-                                .stream()
-                                .map(stopTime -> Report.build(message, stopTime, time))
-                                .collect(Collectors.toList())))
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
 
     @PostMapping("/reports")
     public ResponseEntity<Report> buildReport(@RequestBody String message) {
         var r = reportService.analyse(message, ZonedDateTime.now());
 
-        return r.map(report -> ResponseEntity.ok().body(report))
+        return r.map(report -> ok().body(report))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PostMapping("/reports/save")
+    public ResponseEntity<Report> saveReport(@RequestBody String message) {
+        var r = reportService.analyse(message, ZonedDateTime.now());
+
+        r.ifPresent(reportRepository::create);
+
+        return r.map(ResponseEntity::ok)
+                .orElseGet(() -> badRequest().build());
+    }
+
+
 }
