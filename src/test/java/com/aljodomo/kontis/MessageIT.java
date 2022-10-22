d9a8f30ba6f6da6b7a3b3a84358635cc4d6b4aef
Aljoscha Domonell <aljo.domo@gmail.com>
2022-10-22 17:18:44 +0200

    feat: tests with route name checks

--- src/test/java/com/aljodomo/kontis/MessageIT.java
+++ src/test/java/com/aljodomo/kontis/MessageIT.java
@@ -29,7 +29,7 @@ class MessageIT {
     @Autowired
     GTFSService gtfsService;
 
-    void testComplete(String message, String timeS, String stop) {
+    void testComplete(String message, String timeS, String stop, String routeName) {
         var time = ZonedDateTime.of(LocalDateTime.parse(timeS), TimeZone.getDefault().toZoneId());
         var report = reportService.analyse(message, time);
 
@@ -42,25 +42,26 @@ class MessageIT {
                 "There was no specified StopTimeId present even tho all necessary information is present");
         assertEquals(stop, report.get().getStopName(),
                 "The expected stop name is not the one identified");
+        assertEquals(routeName, report.get().getRouteName(),
+                "The expected route name is not the one identified");
     }
 
-    void testPartial(String message, String timeS, String stop) {
+    void testPartial(String message, String timeS, String stop, String routeName) {
         var time = ZonedDateTime.of(LocalDateTime.parse(timeS), TimeZone.getDefault().toZoneId());
         var report = reportService.analyse(message, time);
 
-        assertTrue(report.isPresent());
-        assertNull(report.get().getStopTimeId());
-        assertEquals(stop, report.get().getStopName());
-    }
+        assertTrue(report.isPresent(),
+                "No report was build even tho all necessary information is present");
 
-    void test(String message, String timeS, int exStopTimeId) {
-        var time = ZonedDateTime.of(LocalDateTime.parse(timeS), TimeZone.getDefault().toZoneId());
-        var expected = gtfsService.getStore().getStopTimeForId(exStopTimeId);
-        var report = reportService.analyse(message, time);
+        log.info(report.get().toString());
 
-        assertTrue(report.isPresent());
+        assertNull(report.get().getStopTimeId(),
+                "There was a StopTimeId present even tho not all necessary information was given");
+        assertEquals(stop, report.get().getStopName(),
+                "The expected stop name is not the one identified");
+        assertEquals(routeName, report.get().getRouteName(),
+                "The expected route name is not the one identified");
 
-        Assertions.assertEquals(expected.getId(), report.get().getStopTimeId());
     }
 
     /**
@@ -68,32 +69,37 @@ class MessageIT {
      */
     @Test
     void complete_message_expect_schlossstr() {
-        testComplete("2 männlich gelesen U9 richtung Rathaus Steglitz schloßstr raus", "2022-02-21T17:39:00", "U Schloßstr. (Berlin)");
+        testComplete("2 männlich gelesen U9 richtung Rathaus Steglitz schloßstr raus", "2022-02-21T17:39:00", "U Schloßstr. (Berlin)", "U9");
+    }
+
+    @Test
+    void complete_message_expect_schichauweg() {
+        testComplete("S 2 Richtung Lichtenrade. Schichauweg jetzt 2 Frauen, Pink und Graue Jacke", "2022-02-21T17:39:00", "S Schichauweg (Berlin)", "S2");
     }
 
     @Test
     void partial_message_expect_tempelhof() {
-        testPartial("Ring 41, die steigen gleich in Tempelhof aus\n", "2022-02-21T17:39:00", "S+U Tempelhof (Berlin)");
+        testPartial("Ring 41, die steigen gleich in Tempelhof aus\n", "2022-02-21T17:39:00", "S+U Tempelhof (Berlin)", "S41");
     }
 
     @Test
     void partial_message_expect_zoologischer() {
-        testPartial("Zoologischer 3 kontrolletis", "2022-02-21T17:39:00", "S+U Zoologischer Garten Bhf (Berlin)");
+        testPartial("Zoologischer 3 kontrolletis", "2022-02-21T17:39:00", "S+U Zoologischer Garten Bhf (Berlin)", null);
     }
 
     @Test
     void complete_message_expect_zoologischer2() {
-        testComplete("U9 direction rathaus steglitz, now at zoologischere garten", "2022-02-21T17:39:00", "S+U Zoologischer Garten Bhf (Berlin)");
+        testComplete("U9 direction rathaus steglitz, now at zoologischere garten", "2022-02-21T17:39:00", "S+U Zoologischer Garten Bhf (Berlin)", "U9");
     }
 
     @Test
     void complete_message_expect_hansaplatz() {
-        testComplete("U9 Hansaplatz richtung Osloer 3 mänlich gelesen und 2weiblich mit schwarzen jacken und dunkle Haare", "2022-02-21T17:39:00", "U Hansaplatz (Berlin)");
+        testComplete("U9 Hansaplatz richtung Osloer 3 mänlich gelesen und 2weiblich mit schwarzen jacken und dunkle Haare", "2022-02-21T17:39:00", "U Hansaplatz (Berlin)", "U9");
     }
 
     @Test
     void complete_message_expect_nordbahnhof() {
-        testComplete("s25 richtung südkreuz jetzt nordbahnhof", "2022-02-21T17:39:00", "S Nordbahnhof (Berlin)");
+        testComplete("s25 richtung südkreuz jetzt nordbahnhof", "2022-02-21T17:39:00", "S Nordbahnhof (Berlin)", "S25");
     }
 
     /**
@@ -101,6 +107,6 @@ class MessageIT {
      */
     @Test
     void complete_message_expect_treptower() {
-        testComplete("3 kontrolletis grade am Treptower park ausgestiegen. Kamen aus der Ringbahn Richtung Frankfurter Allee.", "2022-02-21T17:39:00", "S Treptower Park (Berlin)");
+        testComplete("3 kontrolletis grade am Treptower park ausgestiegen. Kamen aus der Ringbahn Richtung Frankfurter Allee.", "2022-02-21T17:39:00", "S Treptower Park (Berlin)", "S42");
     }
 }
